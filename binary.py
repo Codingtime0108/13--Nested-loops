535db9f227bf2d4d277c64192419831648e5a824
Codingtime <13tia.vora@dulwich-seoul.kr>
2024-10-26 18:53:06 +0900

    binary

--- binary.py
+++ binary.py
@@ -1,1 +1,4 @@
 #binary conversion, how to do modules #decimal to binary
+number = int(input("Enter a number: "))
+binary_representation = bin(number)
+print("Binary representation:", binary_representation)
